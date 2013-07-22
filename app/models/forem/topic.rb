c67f79d751de8765225a344c7492a67dc11e508a
Leo <leo@parwinr.com>
2013-07-22 10:38:01 -1000

    Add last post information to topics index

--- app/models/forem/topic.rb
+++ app/models/forem/topic.rb
@@ -4,7 +4,15 @@ module Forem
     accepts_nested_attributes_for :posts
     belongs_to :user, :class_name => Forem::Engine.user_class.to_s
 
+    #has_one :last_post, :class_name => "Forem::Post",
+    #        :order => "created_at DESC"
+
     before_save :set_post_user
+
+    def last_post
+      Post.order("created_at").last
+    end
+
     private
     def set_post_user
       self.posts.first.user = self.user
