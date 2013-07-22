09a9efd5d18f6df58fe4d2b8aa3a249a0831cf28
Leo <leo@parwinr.com>
2013-07-21 16:26:47 -1000

    Initial base for the forem engine

---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = 'e93bce325717fad60ec38461608ab1d90022ec30054b1628d6ee25dc4215b357e995a162e7fbd50e7570e3e16ba4b04c4432bc860f3f8aa996c9f2e4b77307b3'
